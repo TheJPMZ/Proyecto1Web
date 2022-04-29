11c6d53cfdedc358013285c4f2fee4b902fc97ae
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-28 22:59:41 -0600

    ➕ Background Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import TopPart from "./TopPart";
 import BotPart from "./BotPart";
 import Carrousel from "./Carrousel";
+import Backimage from "./Backimage";
 import "./App.scss";
 
 export default function App(){
     return (
         <>
             <TopPart/>
+            <Backimage/>
+            <Carrousel/>
+            <Carrousel/>
+            <Carrousel/>
             <Carrousel/>
             <Carrousel/>
             <BotPart/>
